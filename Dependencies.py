85b8ad6a9d209b00c8975a5b57e201910faa20cf
Christophe <pc57plv@eduvaud.ch>
2024-08-20 21:45:38 +0200

    Main Menu Base Done + Icon Changed with the player model

---
+++ Dependencies.py
@@ -0,0 +1,8 @@
+###
+# This file is for all the dependencies for Pac Man
+# Last Edited :
+# Made by Christophe & Zachary
+# SI-C3A
+###
+
+import pygame
--- MainMenu.py
+++ MainMenu.py
@@ -1,4 +1,46 @@
 ###
 # This file is for the Main Menu of our game : Pac Man
 # Last Edited :
+# Made by Christophe & Zachary
+# SI-C3A
 ###
+
+from Dependencies import *
+
+pygame.init()
+# Set up the window dimensions
+window_width = 800
+window_height = 600
+
+# Create the screen object
+screen = pygame.display.set_mode((window_width, window_height))
+
+# Set the title of the window
+pygame.display.set_caption("Pac Man by Christophe & Zachary")
+
+# Set the icon of the window
+gameicon = pygame.image.load('Assets/Icon.png')
+
+pygame.display.set_icon(gameicon)
+
+# Main loop
+def main_loop():
+    running = True
+    while running:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill the screen with a color (RGB)
+        screen.fill((0, 0, 0))  # Fill the screen with black
+
+        # Update the display
+        pygame.display.flip()
+
+
+# Using the loop for the game
+main_loop()
+
+# Quit Pygame
+pygame.quit()
